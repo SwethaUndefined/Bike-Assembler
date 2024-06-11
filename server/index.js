d1a295a459a815a8d7031b29e435520a637caf18
“Swetha”Undefined <ramyaragu407@gmail.com>
2024-06-11 10:26:58 +0530

    Changing into MongoDB atlas

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const authRoutes = require("./routes");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,7 +20,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 mongoose
-  .connect("mongodb://localhost:27017/bike-assembler", {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
